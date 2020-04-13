3917efa4ca6e15f4c0c33b22793d9b66c7f47fac
Quentin Aoustin <quentin.aoustin@live.fr>
2020-04-13 18:36:10 +0200

    adding launching icon and pop to ask for token if not entered

--- lib/myPlan.dart
+++ lib/myPlan.dart
@@ -31,7 +31,6 @@ class MyPlanStateful extends State<MyPlan> {
 
   initNotifications() async {
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-    // If you have skipped STEP 3 then change app_icon to @mipmap/ic_launcher
     var initializationSettingsAndroid = new AndroidInitializationSettings('@mipmap/launcher_icon');
     var initializationSettingsIOS = new IOSInitializationSettings();
     var initializationSettings = new InitializationSettings(initializationSettingsAndroid, initializationSettingsIOS);
